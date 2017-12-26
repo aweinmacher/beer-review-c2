2be9e0ec5210879ae11d48e7221341bb486ccc37
Anna W <aweinmacher@gmail.com>
2017-12-26 11:19:24 +0200

    rating sort added

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
 var mats = [];
+var sortclick = 0;
 
 function addMat(name, category) {
+    var rating = $(".rating").val();
     mats.push(
         {
             "name": name,
-            "category": category
+            "category": category,
+            "rating": rating
         }
     );
     console.log(mats);
 }
 
+function sort() {
+   if(sortclick%2===0){
+       mats.sort(function(a,b) {
+            return b.rating-a.rating;
+        });
+   }
+   else{
+       mats.sort(function(a,b) {
+           return a.rating-b.rating;
+       });
+   }
+   renderMats();
+   console.log(mats);
+   sortclick++;
+}
+
 function renderMats() {
     $(".mats-list").empty();
     for (var mat in mats) {
-        $(".mats-list").append("<li>" + mats[mat].name + ", " + mats[mat].category + "</li>");
+        $(".mats-list").append("<li><h4>" + mats[mat].name + ", " + mats[mat].category + ", rating: " + mats[mat].rating + "</h4></li>");
     }
 }
 
@@ -24,3 +43,4 @@ $(".post-mat").click(function () {
     renderMats();
 })
 
+$(".sort").click(sort);
